7765da99f87079cf61a2675467f51e3bb6ab8872
Filipe Santiago <porto.santiago@hotmail.com>
2021-03-20 13:16:33 -0300

    add morgan

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/all-exception.filter';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
+    const logger = new Logger('Application');
 
     app.setGlobalPrefix('api');
 
     app.use(helmet());
+    app.use(
+        morgan('combined', {
+            stream: {
+                write(message) {
+                    logger.log(message);
+                },
+            },
+        }),
+    );
 
     app.useGlobalPipes(
         new ValidationPipe({
